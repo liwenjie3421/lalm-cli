66b5ba48742a412f4e6d4bcb2c2d66bc2255f22e
somnus <somnus@Somnus.local>
2019-07-11 15:47:41 +0800

    node_modules

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log(123)
+import * as commander from 'commander'
+import * as inquirer from 'inquirer'
+import * as download from 'download-git-repo'
+import chalk from 'chalk'
+import ora from 'ora'
+
+// inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             name: 'test',
+//             message: 'r u ok',
+//             default: 'ok'
+//         }
+//     ])
+//     .then(answers => {
+//         console.log(answers)
+//     })
+
+// console.log(chalk.blue('1234'))
+
+// let spinner = ora('downloading template ...')
+// spinner.start()
+// setTimeout(() => {
+//     spinner.stop()
+// }, 5000)
+
+
+// download('liwenjie3421/lalm-cli#master', './template', { clone: false }, (err) => {
+//     console.log(err || 'download success')
+// })
