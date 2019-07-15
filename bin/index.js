f3304e5e50f5aa7b8f51688ee4bff78508772257
liwenjie <18061753421@163.com>
2019-07-15 14:23:37 +0800

    fixbug

--- bin/index.js
+++ bin/index.js
@@ -9,75 +9,22 @@ const download = require('download-git-repo')
 const chalk = require('chalk')
 const ora = require('ora')
 
+const { delDir, copyDir } = require('../lib/utils')
+
 commander.parse(process.argv)
 
-function delDir(path) {
-    let files = []
-    if (fs.existsSync(path)) {
-        files = fs.readdirSync(path)
-        files.forEach((file, index) => {
-            let curPath = `${path}/${file}`
-            if (fs.statSync(curPath).isDirectory()) {
-                delDir(curPath)
-            } else {
-                fs.unlinkSync(curPath)
-            }
-        })
-        fs.rmdirSync(path)
-    }
-}
-
-function copyDir(src, dist, callback) {
-    fs.access(dist, (err) => {
-        if (err) {
-            // 目录不存在时创建目录
-            fs.mkdirSync(dist)
-        }
-        _copy(null, src, dist)
-    })
-
-    function _copy(err, src, dist) {
-        if (err) {
-            callback(err)
-        } else {
-            fs.readdir(src, (err, paths) => {
-                if (err) {
-                    callback(err)
-                } else {
-                    paths.forEach((path) => {
-                        const _src = src + '/' + path
-                        const _dist = dist + '/' + path
-                        fs.stat(_src, (err, stat) => {
-                            if (err) {
-                                callback(err)
-                            } else {
-                                // 判断是文件还是目录
-                                if (stat.isFile()) {
-                                    fs.writeFileSync(_dist, fs.readFileSync(_src))
-                                } else if (stat.isDirectory()) {
-                                    // 当是目录是，递归复制
-                                    copyDir(_src, _dist, callback)
-                                }
-                            }
-                        })
-                    })
-                }
-            })
-        }
-    }
-}
-
-(async () => {
+;(async () => {
     const url = commander.args[0].replace(/^.*github.com\//, '')
-    const templatePath = path.join(__dirname, '../template')
+    console.log('__dirname', __dirname)
+    const templatesPath = path.join(__dirname, '../template')
 
-    fs.existsSync(templatePath) && delDir(templatePath)
+    fs.existsSync(templatesPath) && delDir(templatesPath)
     let spinner = ora()
     spinner.start(chalk.yellow('downloading template ...'))
 
     try {
         await new Promise((resolve, reject) => {
-            download(url, templatePath, { clone: false }, (err) => {
+            download(url, templatesPath, { clone: false }, (err) => {
                 if (err) {
                     reject(err)
                 } else {
@@ -97,12 +44,14 @@ function copyDir(src, dist, callback) {
                 choices: templates
             }
         ])
-        const newTemplatePath = path.join(process.cwd(), `./template/templates/${template}`)
+        const newTemplatePath = path.join(templatesPath, `/templates/${template}`)
         const distPath = path.join(process.cwd(), commander.args[1])
         delDir(distPath)
+        return
         fs.mkdirSync(distPath)
-        
+
         spinner.start(chalk.yellow(`copying template to ${commander.args[1]}`))
+
 
         copyDir(newTemplatePath, commander.args[1], (err) => {
             if (err) {
@@ -113,18 +62,11 @@ function copyDir(src, dist, callback) {
         spinner.stop()
         console.log(chalk.green(`create ${commander.args[1]} success`))
         console.log(`
-            you can: \n
-            $ cd ${distPath}\n
-            $ npm install \n
-        `)
-
-
-
-
+        you can: \n
+        $ cd ${distPath}\n
+        $ npm install \n
+    `)
     } catch (err) {
         spinner.fail(chalk.red(err.toString()))
     }
-
-
-
 })()
